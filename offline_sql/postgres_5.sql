989ae2e4da9204bb3c8aefbef9025270505d1c8c
fuhao1234 <15432459+fuhao1234@user.noreply.gitee.com>
2025-10-08 08:57:12 +0800

    add xm start

--- offline_sql/postgres_5.sql
+++ offline_sql/postgres_5.sql
@@ -1,20 +1,23 @@
 CREATE DATABASE pg_db;
 \c pg_db;
 
-CREATE TABLE user_info (
-                           id BIGSERIAL PRIMARY KEY,
-                           username VARCHAR(50) NOT NULL,
-                           register_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                           status INT NOT NULL DEFAULT 1
+CREATE TABLE user_info
+(
+    id            BIGSERIAL PRIMARY KEY,
+    username      VARCHAR(50) NOT NULL,
+    register_time TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status        INT         NOT NULL DEFAULT 1
 );
 
-CREATE TABLE order_info (
-                            order_id BIGSERIAL PRIMARY KEY,
-                            user_id BIGINT NOT NULL,
-                            amount DECIMAL(10,2) NOT NULL,
-                            create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                            FOREIGN KEY (user_id) REFERENCES user_info(id)
+CREATE TABLE order_info
+(
+    order_id    BIGSERIAL PRIMARY KEY,
+    user_id     BIGINT         NOT NULL,
+    amount      DECIMAL(10, 2) NOT NULL,
+    create_time TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user_info (id)
 );
 
-ALTER SYSTEM SET wal_level = hot_standby;
-SELECT * FROM pg_create_logical_replication_slot('pg_cdc_slot', 'pgoutput');
+ALTER SYSTEM SET wal_level = logical;
+SELECT *
+FROM pg_create_logical_replication_slot('pg_cdc_slot', 'pgoutput');
